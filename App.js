b343ee530634fcccf558547c324ce502318608ad
deepali kumari <111889695+kumariDeepali@users.noreply.github.com>
2022-12-06 20:47:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+
+import React from "react";
+//import "../styles/App.css";
+const App = () => {
+  const handleDoubleClick = (event) => {
+    console.log("I was double clicked");
+  };
+
+  function singel() {
+    console.log("I was not double clicked");
+  }
+
+  return (
+    <div id="main">
+      <button
+        id="dblclick-btn"
+        onClick={singel}
+        onDoubleClick={handleDoubleClick}
+      >
+        Double click me
+      </button>
+    </div>
+  );
+};
+
+export default App;
